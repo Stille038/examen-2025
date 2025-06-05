62993930f88e2641825f78b6a7961477971116ea
Danila_gruznev <92578919+Stille038@users.noreply.github.com>
2025-06-05 14:53:32 +0200

    fix testbranch

--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,16 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Api\AanwezigheidController;
 use App\Models\Aanwezigheid;
 use App\Http\Controllers\ExcelUploadController;
 use App\Http\Controllers\RapportageController;
+use App\Http\Controllers\AanwezigheidController;
+use App\Http\Controllers\CustomLoginController;
+
 
 Route::get('/aanwezigheden', [AanwezigheidController::class, 'index']);
+Route::post('/custom-login', [CustomLoginController::class, 'login'])->name('custom.login');
+Route::redirect('/', '/login');
 
 Route::get('/', function () {
     $studenten = Aanwezigheid::all(); // haal alle studentgegevens op
@@ -15,29 +19,31 @@ Route::get('/', function () {
 })->name('dashboard');
 
 Route::get('/student-dashboard', function () {
-    return view('student-dashboard'); 
+    return view('student-dashboard');
 })->name('student-dashboard');
 Route::get('/individueel-student', function () {
-    return view('individueel-student'); 
+    return view('individueel-student');
 })->name('individueel-student');
 Route::get('/test1', function () {
-    return view('test1'); 
+    return view('test1');
 })->name('test1');
 
 Route::get('/test2', function () {
-    return view('test2'); 
+    return view('test2');
 })->name('test2');
 
 Route::get('/test', function () {
-    return view('test'); 
+    return view('test');
 })->name('test');
 
 Route::get('/docent/dashboard', function () {
     $studenten = \App\Models\Aanwezigheid::all();
     // Dynamisch: groepeer op groep en bereken gemiddelden
-    $groepen = $studenten->groupBy('groep')->map(function($groepStudenten, $groepNaam) {
+    $groepen = $studenten->groupBy('groep')->map(function ($groepStudenten, $groepNaam) {
         $aantal = $groepStudenten->count();
-        $gemiddelde = $aantal > 0 ? round($groepStudenten->avg(function($s) { return $s->rooster ? ($s->aanwezigheid / $s->rooster) * 100 : 0; }), 0) : 0;
+        $gemiddelde = $aantal > 0 ? round($groepStudenten->avg(function ($s) {
+            return $s->rooster ? ($s->aanwezigheid / $s->rooster) * 100 : 0;
+        }), 0) : 0;
         return [
             'naam' => $groepNaam,
             'gemiddelde' => $gemiddelde,
@@ -65,4 +71,4 @@ Route::post('/upload-excel', [ExcelUploadController::class, 'store'])->name('exc
 Route::get('/docent/rapportage/student/{studentnummer}', [RapportageController::class, 'studentPdf'])->name('rapportage.student.pdf');
 Route::get('/docent/rapportage/groep/{groepNaam}', [RapportageController::class, 'groepPdf'])->name('rapportage.groep.pdf');
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
